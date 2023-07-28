1e5a54ff4b2eeaa1f6a48000bfcc5c308ca6f7aa
Yash <kudveyash@gmail.com>
2023-07-28 15:25:21 +0530

    Template for project

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import { VirtualizedList } from 'react-native-web'
+
+const Home = () =>{
+    return(
+        <View style={{alignItems:'center'}}>
+            <Text style={{alignContent:'center'}}>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
