b179e91d8975065444a2da166f5fd032ab371ec7
Yash <kudveyash@gmail.com>
2023-07-31 20:00:56 +0530

    adding various components

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
-import {View, Text} from 'react-native'
+import {View, Text, ScrollView, SafeAreaView} from 'react-native'
+import {Stack, useRouter} from 'expo-router'
 import { VirtualizedList } from 'react-native-web'
+import {COLORS,icons, images,SIZES} from '../constants'
+import {Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome} from '../components'
 
 const Home = () =>{
+    const router = useRouter();
     return(
-        <View style={{alignItems:'center'}}>
+        <SafeAreaView style={{alignItems:'center', flex:1, backgroundColor:COLORS.lightWhite}}>
+
             <Text style={{alignContent:'center'}}>Home</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
